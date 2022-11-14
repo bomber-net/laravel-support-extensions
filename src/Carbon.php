d25ce791b14c2f38729932413acaf4a51484401e
bomber-net <an-rtk@mail.ru>
2022-11-14 23:06:47 +0300

    optimize requires in composer.json

--- src/Carbon.php
+++ src/Carbon.php
@@ -2,10 +2,12 @@
 
 namespace BomberNet\LaravelSupportExtensions;
 
+use Illuminate\Support\Facades\Date;
+
 class Carbon extends \Illuminate\Support\Carbon
 	{
 		public static function makeNotNull ($var):\Illuminate\Support\Carbon
 			{
-				return parent::make ($var)??now ();
+				return parent::make ($var)??Date::now ();
 			}
 	}
