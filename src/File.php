d3ab2fabfa766bc4b04c23ee7c0fa2d924153cbf
bomber-net <an-rtk@mail.ru>
2023-01-05 11:59:43 +0300

    small optimizations in \BomberNet\LaravelSupportExtensions\File

--- src/File.php
+++ src/File.php
@@ -2,11 +2,13 @@
 
 namespace BomberNet\LaravelSupportExtensions;
 
+use Illuminate\Support\Facades\Facade;
+
 /**
  * @method static bool absent (string $path)
  * @method static mixed nextFile (string $dir,string $pattern='*.*',string $cursorFile='current.file')
  */
-class File extends \Illuminate\Support\Facades\File
+class File extends Facade
 	{
 		protected static function getFacadeAccessor ():string
 			{
